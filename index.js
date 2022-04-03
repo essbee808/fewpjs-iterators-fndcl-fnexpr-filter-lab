34322e044a7750a42c3363c376c71e173d313954
Sheena Bonilla <sheena.myb@gmail.com>
2022-04-03 12:13:26 -0700

    commit tree 3086916a1d790f7fea0985757a5a5be8028ac22b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, name) {
+    let arr = drivers.filter(n => n.toUpperCase() === name.toUpperCase() )
+    if (arr !== undefined) {
+        return arr
+    }
+}
+
+// return all drivers whose names begin with provided letters
+function fuzzyMatch(drivers, name) {
+
+    let arr = drivers.filter(n => {
+        // debugger;
+        return n.charAt(0).toUpperCase() === name.charAt(0).toUpperCase() 
+    })
+    if (arr !== undefined) {
+        return arr
+    }
+}
+
+function matchName(drivers, name) {
+    return drivers.filter( n => n.name === name)
+}
